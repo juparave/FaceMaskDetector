b8e251fa9fd4f68b1de210834f4fbea4a07a217c
Juan Pablo TankerMini <juparave@deuxbits.com>
2021-06-22 18:52:37 -0500

    Transport

--- facemask.py
+++ facemask.py
@@ -70,6 +70,8 @@ mymodel=load_model('mymodel.h5')
 #test_image=image.load_img('C:/Users/Karan/Desktop/ML Datasets/Face Mask Detection/Dataset/test/without_mask/30.jpg',target_size=(150,150,3))
 test_image=image.load_img(r'./test/with_mask/1-with-mask.jpg',
                           target_size=(150,150,3))
+test_image=image.load_img(r'./test/without_mask/26.jpg',
+                          target_size=(150,150,3))
 test_image
 test_image=image.img_to_array(test_image)
 test_image=np.expand_dims(test_image,axis=0)
